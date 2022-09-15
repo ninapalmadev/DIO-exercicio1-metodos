607eb589b3032fac0715761a8f682662dd564a2b
Nina Palma <ninapalmadev@gmail.com>
2022-09-15 17:01:02 -0300

    cria e altera métodos para o exercício pedido

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,25 +1,25 @@
 public class Calculadora {
 
-    public static void Soma(double numero1, double numero2){
+    public static void soma(double numero1, double numero2){
 
         double resultado = numero1 + numero2;
 
         System.out.println("A soma de: " + numero1 + " + " + numero2 + " é igual a: " + resultado);
     }
-    public static void Subtracao(double numero1, double numero2){
+    public static void subtracao(double numero1, double numero2){
 
         double resultado = numero1 - numero2;
 
         System.out.println("A subtração de: " + numero1 + " - " + numero2 + " é igual a: " + resultado);
     }
-    public static void Multiplicacao(double numero1, double numero2){
+    public static void multiplicacao(double numero1, double numero2){
 
         double resultado = numero1 * numero2;
 
         System.out.println("A multiplicação de: " + numero1 + " X " + numero2 + " é igual a: " + resultado);
     }
 
-    public static void Divisao(double numero1, double numero2){
+    public static void divisao(double numero1, double numero2){
 
         double resultado = numero1 / numero2;
 
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        //Calculadora
+        System.out.println("Exercício calculadora");
+        Calculadora.soma(7,6);
+        Calculadora.subtracao(14, 1.8);
+        Calculadora.multiplicacao(3, 9);
+        Calculadora.divisao(30, 8.5);
+
+        // Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(8);
+        Mensagem.obterMensagem(16);
+        Mensagem.obterMensagem(21);
+        Mensagem.obterMensagem(2);
+
+        // Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(2000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(2000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(2000, 6);
+    }
 }
